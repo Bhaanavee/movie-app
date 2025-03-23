94bf6520e4b8870c3f0c0fb0d3b856b781ed35b6
Bhaanavee <bhanavee123@gmail.com>
2025-03-24 00:28:20 +0530

    TMDB

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import heroImage from "./assets/hero.png";
 import Search from "./components/Search";
+
+const API_BASE_URL = 'https://api.themoviedb.org/3'
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 function App() {
 
   const [searchTerm, setSearchTerm] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const fetchMovies = async () => {
+    try{
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if(!response.ok) {
+        throw new Error('Failed to fetch movies');
+      }
+
+      const data = await response.json();
+      
+      if(data.Response === 'False') {
+        setErrorMessage(data.Error || 'Failed to fetch movies');
+      }
+
+    }catch(error){
+      console.error(`Error Fetching Movies : ${error}`)
+      setErrorMessage('Error fetching movies, please try again later'); 
+    };
+    
+
+  }
+
+  useEffect(() => {
+
+  },[]);
   return (
     <main>
       <div className="pattern" />
@@ -16,6 +56,12 @@ function App() {
           </h1>
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </header>
+        <section className="all-movies">
+          <h2>All Movies</h2>
+
+            <p className="text-red-500">{errorMessage}</p>
+          
+        </section>
       </div>
     </main>
   );
