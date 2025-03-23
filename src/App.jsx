0b67f82fffe7438a6d3d64f0f8311b715ac4da3f
Bhaanavee <bhanavee123@gmail.com>
2025-03-23 10:40:38 +0530

    hero page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-const Card = () => {
-  return(
-    <div>
-      <h2>Card Component</h2>
-    </div>
-  )
-}
+import heroImage from "./assets/hero.png";
+import Search from "./components/Search";
 
 function App() {
   return (
-    <>
-    <h2>Rendering h2</h2>
-    <Card />
-    <Card />
-    </>
-    
-    
-
-  )
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src={heroImage} alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle
+          </h1>
+          <Search />
+        </header>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#030014",
+          light100: "#cecefb",
+          light200: "#a8b5db",
+          gray100: "#9ca4ab",
+          dark100: "#0f0d23",
+        },
+      },
+    },
+    plugins: [],
+  };
+  
